21638285cf329622068fa8bce0429b6414bb0a16
Swyamhp <swayamgd0@gmail.com>
2025-04-20 10:12:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+// Smooth scrolling for all anchor links in navbar
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+anchor.addEventListener('click', function (e) {
+e.preventDefault();
+
+const targetId = this.getAttribute('href').substring(1); // Remove #
+const targetSection = document.getElementById(targetId);
+
+if (targetSection) {
+  targetSection.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+});
+});
